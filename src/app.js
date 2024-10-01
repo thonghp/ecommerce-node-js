620fc94b6cb1f4ef9e144fdbdd636fc4a115c475
thonghp <thonghoangpham123@gmail.com>
2024-10-01 23:06:14 +0700

    add middleware morgan helmet compression

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const compression = require("compression");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev")); // log
+app.use(helmet()) // security lọc http chặn đọc cookie
+app.use(compression()) // giảm bớt băng thông
+
+// init db
+// require('./dbs/init.mongodb.lv0')
+
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompress = 'repeat 100000'
+
+  return res.status(200).json({
+    message: "hello",
+    metadata: strCompress.repeat(10000)
+  });
+});
+// handling error
+
+module.exports = app;
